1efabc167d6441862dc7180614e896a1dab679da
samuel vermeuln <vermeuln@ucl.br>
2025-05-18 12:41:10 -0300

    exemple

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import {getPayload} from 'payload'
+import configPromise from '@payload-config'
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const payload = await getPayload({config: configPromise})
+
+  const documents = await payload.find({
+    collection: 'documents',
+  }).then(res => res)
+
+    console.log("documents =>>",documents)
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
